a88d2c7e1b9185710ce1f03472296d9fd2307b03
houxiaofan <houxiaofan.321@163.com>
2022-09-20 11:02:48 +0800

    idea third commit

--- src/main/java/com/fenghe/test.java
+++ src/main/java/com/fenghe/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("git first sysout");
         System.out.println("git second sysout");
+        System.out.println("git third sysout");
     }
 }
